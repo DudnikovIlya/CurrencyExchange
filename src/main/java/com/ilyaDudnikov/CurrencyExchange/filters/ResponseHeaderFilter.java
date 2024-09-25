aa744d08331269780e84c1b70d195754178c594f
ilyad <iladudnikov13@gmail.com>
2024-09-26 01:24:38 +0700

    Added ResponseHeaderFilter

---
+++ src/main/java/com/ilyaDudnikov/CurrencyExchange/filters/ResponseHeaderFilter.java
@@ -0,0 +1,19 @@
+package com.ilyaDudnikov.CurrencyExchange.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.MediaType;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ResponseHeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
+        httpResp.setCharacterEncoding(StandardCharsets.UTF_8.name()); // UTF-8
+        httpResp.setContentType(MediaType.APPLICATION_JSON); // application/json
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
--- src/main/java/com/ilyaDudnikov/CurrencyExchange/servlets/JsonWriter.java
+++ src/main/java/com/ilyaDudnikov/CurrencyExchange/servlets/JsonWriter.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import jakarta.ws.rs.core.MediaType;
 
 public class JsonWriter {
     private final ObjectMapper objectMapper;
@@ -18,8 +16,6 @@ public class JsonWriter {
 
     public void writeToResponse(HttpServletResponse response, Object obj, int statusCode) throws IOException {
         response.setStatus(statusCode);
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name()); // UTF-8
-        response.setContentType(MediaType.APPLICATION_JSON); // application/json
 
         String jsonStr = objectMapper.writeValueAsString(obj);
         response.getWriter().write(jsonStr);
