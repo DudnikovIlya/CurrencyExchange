6d927551f31f89bc64ff38c80ffb65eea169d666
ilyad <iladudnikov13@gmail.com>
2024-09-28 18:10:11 +0700

    fix:Added annotation to ResponseHeaderFilter

--- src/main/java/com/ilyaDudnikov/CurrencyExchange/filters/ResponseHeaderFilter.java
+++ src/main/java/com/ilyaDudnikov/CurrencyExchange/filters/ResponseHeaderFilter.java
@@ -1,12 +1,14 @@
 package com.ilyaDudnikov.CurrencyExchange.filters;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@WebFilter("/*")
 public class ResponseHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
